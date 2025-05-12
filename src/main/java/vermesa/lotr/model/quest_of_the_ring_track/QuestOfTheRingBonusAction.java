71708cf42d9c9360b8bfafd8e33d7b5404194386
Vermes Ákos <vermihunter@gmail.com>
2025-05-13 00:57:17 +0200

    Game state evaluation

---
+++ src/main/java/vermesa/lotr/model/quest_of_the_ring_track/QuestOfTheRingBonusAction.java
@@ -0,0 +1,17 @@
+package vermesa.lotr.model.quest_of_the_ring_track;
+
+import vermesa.lotr.model.game.GameContext;
+import vermesa.lotr.model.game.GameState;
+import vermesa.lotr.model.actions.ActionResult;
+import vermesa.lotr.model.actions.IAction;
+
+import java.util.List;
+
+public record QuestOfTheRingBonusAction(int pos, List<IAction> action) implements IAction {
+
+    @Override
+    public ActionResult action(GameContext ctx, GameState state) {
+
+        return null;
+    }
+}
